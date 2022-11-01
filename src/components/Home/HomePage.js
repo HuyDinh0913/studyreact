a1c56053c5c237a95683882b70a83c4a4403cb01
HUY DINH <huydinhthanh@gmail.com>
2022-11-01 12:49:41 +0700

    video52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -8,6 +8,12 @@ const HomePage = (props) => {
                     type="video/mp4"
                 />
             </video>
+            <div className='homepage-content'>
+                <div className='title-big'>There's a better way to ask</div>
+                <div className='title-small'>You don't want to make a boring form. And your audience won't answer one. Create a typeform instead—and make everyone happy.</div>
+                <div className='title-bt'><button>Get started-it's free</button></div>
+            </div>
+
         </div>
     )
 }
